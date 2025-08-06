a6c210ed344893b71c857072353701f09743adeb
chw1230 <choihw1230@naver.com>
2025-08-06 15:16:18 +0900

    ✨feat: Store Entity 설계( 정렬을 위한 기준 Enum 설정 )

--- src/main/java/com/study/demo/backend/domain/store/entity/Store.java
+++ src/main/java/com/study/demo/backend/domain/store/entity/Store.java
@@ -1,5 +1,6 @@
 package com.study.demo.backend.domain.store.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.study.demo.backend.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -22,15 +23,26 @@ public class Store extends BaseEntity {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "latitude", nullable = false)
+    @Column(name = "latitude", nullable = false, precision = 9, scale = 6)
     private BigDecimal latitude;
 
-    @Column(name = "longitude", nullable = false)
+    @Column(name = "longitude", nullable = false, precision = 9, scale = 6)
     private BigDecimal longitude;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
     @Column(name = "opening_time", nullable = false)
     private LocalTime openingTime;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
     @Column(name = "closing_time", nullable = false)
     private LocalTime closingTime;
+
+    public void update(String name, BigDecimal latitude, BigDecimal longitude,
+                       LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
 }
