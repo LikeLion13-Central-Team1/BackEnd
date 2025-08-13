3fef4917de0380a926bf1ec44c19db81cb9950e9
Jimyeong Kim <kjm021221@icloud.com>
2025-08-13 19:42:58 +0900

    ♻️refactor: API 배포 URL CorsConfig에 추가

--- src/main/java/com/study/demo/backend/global/config/CorsConfig.java
+++ src/main/java/com/study/demo/backend/global/config/CorsConfig.java
@@ -29,6 +29,8 @@ public class CorsConfig {
         allowedOriginPatterns.add("http://127.0.0.1:5500");
         allowedOriginPatterns.add("http://localhost:63342");
         allowedOriginPatterns.add("http://127.0.0.1:63342");
+        allowedOriginPatterns.add("https://api-whynotbuy.store");
+        allowedOriginPatterns.add("https://www.api-whynotbuy.store");
 
         configuration.setAllowedOriginPatterns(allowedOriginPatterns); // 허용할 Origin 설정
 
