0815016f20eecdd89127240a82a3ed43e0d0985c
CHOI HYUN WOO <101390526+chw1230@users.noreply.github.com>
2025-08-25 22:20:57 +0900

    ♻️ refactor: Order 생성 시간 조건 변경( 서비스 시연을 위한 주문 생성 제한 없애기 )
    ♻️ refactor: Order 생성 시간 조건 변경( 서비스 시연을 위한 주문 생성 제한 없애기 )

--- src/main/java/com/study/demo/backend/domain/order/service/command/OrderCommandServiceImpl.java
+++ src/main/java/com/study/demo/backend/domain/order/service/command/OrderCommandServiceImpl.java
@@ -62,15 +62,7 @@ public class OrderCommandServiceImpl implements OrderCommandService {
                 .orElseThrow(() -> new CustomException(StoreErrorCode.STORE_NOT_FOUND));
 
         // 마감 10시간 전 ~ 마감 시간까지 -> 주문 생성 가능 시간 확인
-        LocalTime closingTime = store.getClosingTime();
-        LocalDate today = LocalDate.now();
-        LocalDateTime todayClosingTime = LocalDateTime.of(today, closingTime);
-        LocalDateTime orderAvailableStartTime = todayClosingTime.minusHours(10);
-
-        LocalDateTime now = LocalDateTime.now();
-        if (now.isBefore(orderAvailableStartTime) || now.isAfter(todayClosingTime)) {
-            throw new CustomException(OrderErrorCode.ORDER_TIME_UNAVAILABLE);
-        }
+        LocalTime closing경
 
         List<Long> menuIdList = createOrder.orderMenus().stream()
                 .map(OrderReqDTO.OrderItem::menuId)
